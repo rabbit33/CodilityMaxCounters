079b90474c92dbda1326bb0a168ed13fd7af2768
Rabbit <alexandra.lavinia.szabo@gmail.com>
2020-04-27 08:50:32 +0300

    Initial commit

---
+++ CodilityMaxCounters/CodilityMaxCounters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodilityMaxCounters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int[] a = new int[7] { 3, 4, 4, 6, 1, 4, 4 };
+
+            Solution s = new Solution();
+            s.solution(n, a);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodilityMaxCounters/CodilityMaxCounters/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodilityMaxCounters
+{
+    public class Solution
+    {
+        public int[] solution(int N, int[] A)
+        {
+            int[] result = new int[N];
+            int m = A.Length;
+
+            int maxCounter = 0;
+            int lastUpdate = 0;
+
+            for (int k = 0; k < m; k++)
+            {
+                if (A[k] == N + 1)
+                {
+                    lastUpdate = maxCounter;
+                }
+                else
+                {
+                    int index = A[k] - 1;
+                    result[index] = Increment(result[index], lastUpdate);
+                    maxCounter = Math.Max(maxCounter, result[index]);
+                }
+            }
+
+            UpdateResult(result, lastUpdate);
+            Print(result);
+
+            return result;
+        }
+
+        private int Increment(int value, int lastUpdate)
+        {
+            return value < lastUpdate ? lastUpdate + 1 : value + 1;
+        }
+
+        private void UpdateResult(int[] result, int lastUpdate)
+        {
+            for (var i = 0; i < result.Length; i++)
+            {
+                if (result[i] < lastUpdate)
+                {
+                    result[i] = lastUpdate;
+                }
+            }
+        }
+
+        public void Print(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
